85a5d61185b0303e8121be5b59540876590e8c80
Osvaldo <torquati.osvaldo@gmail.com>
2023-07-27 00:21:53 -0300

    Update page.tsx
    Update the Footer on project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2 } from 'lucide-react'
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
@@ -117,10 +117,46 @@ export default function Home() {
             </div>
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'>
-        <div></div>
-        <div></div>
-        <div></div>
+      <footer className='bg-zinc-900 border-t border-zinc-700 p-6 flex items-center justify-between'>
+        <div className="flex items-center gap-3">
+          <Image src="/album.jpg" width={32} height={32} alt="Capa do Album da banda" />
+          <div className="flex flex-col">
+            <strong className="font-normal">It's a mistake</strong>
+            <span className="text-xs text-zinc-400">Men at Work</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center" >
+          <div className="flex items-center gap-4">
+            <Shuffle size={20} className="text-zinc-200"/>
+            <SkipBack size={20} className="text-zinc-200"/>
+              <button className="w-10 h-10 pl-1 flex items-center justify-center rounded-full bg-white text-black">
+                <Play />
+              </button>
+            <SkipForward size={20} className="text-zinc-200"/>
+            <Repeat size={20} className="text-zinc-200"/>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400" >0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600" >
+              <div className="h-1 rounded-full bg-zinc-200 w-40"></div>
+            </div>
+            <span className="text-xs text-zinc-400" >2:31</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4" >
+          <Mic2 size={20}/>
+          <LayoutList size={20}/>
+          <Laptop2 size={20}/>
+          <div className="flex items-center gap-2">
+            <Volume size={20}/>
+            <div className="h-1 rounded-full w-24 bg-zinc-600" >
+              <div className="h-1 rounded-full bg-zinc-200 w-10">
+
+              </div>
+            </div>
+          </div>
+          <Maximize2 size={20}/>
+        </div>
       </footer>
     </div>
   )
